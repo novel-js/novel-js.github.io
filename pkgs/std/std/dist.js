9d306403a11e80f0bd0da9fd7218d317ac7a8379
Rusty Shackleford <a@neb.best>
2020-09-26 13:31:53 -0400

    Fixed dist.js

--- pkgs/std/std/dist.js
+++ pkgs/std/std/dist.js
@@ -3,7 +3,7 @@ import {print} from  "https://novel-js.github.io/pkgs/std/std/print.js";
 import {read} from   "https://novel-js.github.io/pkgs/std/std/read.js";
 import {write} from  "https://novel-js.github.io/pkgs/std/std/write.js";
 import {append} from "https://novel-js.github.io/pkgs/std/std/append.js";
-import {exists} from "https://novel-js.github.io/pkgs/std/std/append.js";
+import {exists} from "https://novel-js.github.io/pkgs/std/std/exists.js";
 
 const file = {
     read: read,
