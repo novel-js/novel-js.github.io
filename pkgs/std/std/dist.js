a4b9773dc5afec58de283eb97052554c8b9d37e8
Rusty Shackleford <a@neb.best>
2020-09-26 12:16:21 -0400

    small mistake

--- pkgs/std/std/dist.js
+++ pkgs/std/std/dist.js
@@ -1,7 +1,7 @@
 import {println} from "https://novel-js.github.io/pkgs/std/std/println.js";
 import {print} from "https://novel-js.github.io/pkgs/std/std/print.js";
 import {read} from "https://novel-js.github.io/pkgs/std/std/read.js";
-import {read} from "https://novel-js.github.io/pkgs/std/std/write.js";
+import {write} from "https://novel-js.github.io/pkgs/std/std/write.js";
 
 const file = {
     read: read,
