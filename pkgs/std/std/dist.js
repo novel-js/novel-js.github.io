e406f9cfa7cd890649d4d2e4dfd24aab423a5e2d
Rusty Shackleford <a@neb.best>
2020-09-26 13:31:04 -0400

    Add exists

--- pkgs/std/std/dist.js
+++ pkgs/std/std/dist.js
@@ -1,13 +1,15 @@
-import {println} from "https://novel-js.github.io/pkgs/std/std/println.js";
-import {print} from "https://novel-js.github.io/pkgs/std/std/print.js";
-import {read} from "https://novel-js.github.io/pkgs/std/std/read.js";
-import {write} from "https://novel-js.github.io/pkgs/std/std/write.js";
+import {println} from"https://novel-js.github.io/pkgs/std/std/println.js";
+import {print} from  "https://novel-js.github.io/pkgs/std/std/print.js";
+import {read} from   "https://novel-js.github.io/pkgs/std/std/read.js";
+import {write} from  "https://novel-js.github.io/pkgs/std/std/write.js";
 import {append} from "https://novel-js.github.io/pkgs/std/std/append.js";
+import {exists} from "https://novel-js.github.io/pkgs/std/std/append.js";
 
 const file = {
     read: read,
     write: write,
-    append: append
+    append: append,
+    exists: exists
 }
 export {println, print, file}
 export default {println, print, file}
