1e6f26588ed7de39fcff6472daafce1a285ebaf0
Rusty Shackleford <a@neb.best>
2020-09-26 13:01:09 -0400

    Add append

---
+++ pkgs/std/std/append.js
@@ -0,0 +1,11 @@
+function append(filename, content) {
+    return new Promise((resolve, reject) => {
+        try {
+            resolve($.fs_append(filename, content))
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+export { append }
+export default append
--- pkgs/std/std/dist.js
+++ pkgs/std/std/dist.js
@@ -2,10 +2,12 @@ import {println} from "https://novel-js.github.io/pkgs/std/std/println.js";
 import {print} from "https://novel-js.github.io/pkgs/std/std/print.js";
 import {read} from "https://novel-js.github.io/pkgs/std/std/read.js";
 import {write} from "https://novel-js.github.io/pkgs/std/std/write.js";
+import {append} from "https://novel-js.github.io/pkgs/std/std/append.js";
 
 const file = {
     read: read,
-    write: write
+    write: write,
+    append: append
 }
 export {println, print, file}
 export default {println, print, file}
