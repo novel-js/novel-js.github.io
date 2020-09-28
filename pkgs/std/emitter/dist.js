d7997b2049c45d2d8db6df40d46711e26e6653a8
Rusty Shackleford <a@neb.best>
2020-09-28 17:16:25 -0400

    x

--- pkgs/std/emitter/dist.js
+++ pkgs/std/emitter/dist.js
@@ -1,4 +1,4 @@
-import { eventEmitter } from 'https://novel-js.github.io/pkgs/std/emitter/eventEmitter.js'
+import { Emitter } from 'https://novel-js.github.io/pkgs/std/emitter/eventEmitter.js'
 
-export { eventEmitter }
-export default { eventEmitter }
+export { Emitter }
+export default { Emitter }
